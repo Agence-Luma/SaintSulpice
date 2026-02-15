82a117c26f456f7e47dce2e42a687040a00cb3e6
Evan Martin <contact@evanmartin.fr>
2026-02-14 22:55:45 -0500

    FullWidthText

--- app/slices/FullWidthText/index.vue
+++ app/slices/FullWidthText/index.vue
@@ -10,9 +10,9 @@ defineProps(getSliceComponentProps<Content.FullWidthTextSlice>());
     :data-slice-variation="slice.variation"
     :id="slice.primary.slice_id || ''"
   >
-    <div class="w-full bg-lightblue text-white px-[50px] lg:px-[200px] py-[50px]">
-      <p class="font-bebas uppercase text-xl leading-[100%]">{{ slice.primary.title }}</p>
-      <div class="font-montserrat text-l"><PrismicRichText :field="slice.primary.text" /></div>
+    <div class="w-full bg-lightblue text-white px-[20px] md:px-[50px] lg:px-[200px] py-[50px] text-center md:text-left">
+      <p class="font-bebas uppercase text-l md:text-xl leading-[100%]">{{ slice.primary.title }}</p>
+      <div class="font-montserrat text-m md:text-l"><PrismicRichText :field="slice.primary.text" /></div>
     </div>
   </section>
 </template>
