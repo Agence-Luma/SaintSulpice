b3e97a510e8510bdf46022257ae25b70e8a2fd52
evanmartiin <evan.martiin29@gmail.com>
2026-02-20 21:09:21 +0000

    Build: (5c471be) Add GitHub Actions workflow for build and push

---
+++ _nuxt/_NWalBqq.js
@@ -0,0 +1,1 @@
+import{E as e}from"./BmOeE4H9.js";const o=()=>e().$device;export{o as u};
--- app/utils/prismic.ts
+++
@@ -1,4 +0,0 @@
-export const getPrismicSingle = (id: string) => {
-  const { client } = usePrismic();
-  return useAsyncData(id, () => client.getSingle(id)).data;
-};
