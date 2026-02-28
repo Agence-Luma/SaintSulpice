ee583b43f86a94a15b8671195e489abf710aa511
Evan Martin <evan.martiin29@gmail.com>
2026-02-28 18:54:36 -0500

    hover boutons

--- app/slices/TitleButton/index.vue
+++ app/slices/TitleButton/index.vue
@@ -13,7 +13,7 @@ defineProps(getSliceComponentProps<Content.TitleButtonSlice>());
     <div class="w-full h-[300px] bg-black flex" :class="slice.primary.reverse ? 'flex-row-reverse' : 'flex-row'">
       <div class="w-[500px] h-full bg-white flex flex-col items-center justify-center text-lightblue">
         <p class="font-bebas uppercase text-xl leading-[100%] mb-[10px]">{{ slice.primary.title }}</p>
-        <PrismicLink :field="slice.primary.link" target="_blank" class="bg-lightblue text-white rounded-full px-[50px] py-[5px] text-m font-bold font-montserrat" />
+        <PrismicLink :field="slice.primary.link" target="_blank" class="bg-lightblue text-white rounded-full px-[50px] py-[5px] text-m font-bold font-montserrat border-[2px] border-lightblue hover:text-lightblue hover:bg-white hover:cursor-pointer transition-all" />
       </div>
     </div>
   </section>
